6c793da189a3545bbd1e2e9c3a511bed6703078c
thiagomedinad <thiagomedinad21@gmail.com>
2022-11-14 14:53:03 -0300

    Source code updated

--- src/index.js
+++ src/index.js
@@ -15,13 +15,20 @@ app.get('/', (req, res) => {
     res.json({message: 'API is working!'})
 });
 
-// Routes
+// User CRUD Routes
 app.get('/users', db.getAllUsers);
 app.get('/users/:id', db.getUser);
 app.post('/users', db.addUser);
 app.put('/users/:id', db.updateUserName);
 app.delete('/users/:id', db.deleteUser);
 
+// Product CRUD Routes
+app.get('/products', db.getAllProducts);
+app.post('/products/register', db.addProduct);
+
+// Categorie CRUD Routes
+app.get('/categorie', db.getAllCategories);
+app.post('/categorie/register', db.addCategorie);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);

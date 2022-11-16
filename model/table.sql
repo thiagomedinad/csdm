c5340f74e6a0badd682efc41ca30a0154d212f1d
thiagomedinad <thiagomedinad21@gmail.com>
2022-11-16 09:59:06 -0300

    New tables added

--- model/table.sql
+++ model/table.sql
@@ -1,19 +1,26 @@
-CREATE TABLE users (
-    ID SERIAL PRIMARY KEY,
-    cpf varchar(11) NOT NULL,
-    name varchar(30),
-    email varchar(50)
+CREATE TABLE usuario (
+    ID SERIAL,
+    nome varchar(30) NOT NULL,
+    PRIMARY KEY (ID)
 );
 
 CREATE TABLE produto (
-    ID SERIAL PRIMARY KEY,
+    ID SERIAL,
     preco integer NOT NULL,
-    prazo varchar(10),
-    nota integer DEFAULT 0,
-    nome varchar(20)
+    prazo integer DEFAULT 0 NOT NULL,
+    avaliacao integer DEFAULT 0 NOT NULL,
+    nome varchar(30) NOT NULL
+    PRIMARY KEY (ID)
 );
 
 CREATE TABLE categoria (
-    ID SERIAL PRIMARY KEY,
-    nome varchar(30)
+    ID SERIAL,
+    id_usuario integer NOT NULL,
+    PRIMARY KEY(ID),
+    CONSTRAINT fk_categoria_usuario_id_idx FOREIGN KEY (id_usuario) REFERENCES usuario(ID); 
 );
+
+CREATE TABLE fornecedor (
+    ID SERIAL,
+
+)

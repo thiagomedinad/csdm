f8f271e6809d9efee5da50cee2d054cf61eb324b
thiagomedinad <thiagomedinad21@gmail.com>
2022-11-17 22:11:05 -0300

    u

--- model/table.sql
+++ model/table.sql
@@ -84,3 +84,88 @@ create table interacao_produto (
 	FOREIGN KEY produto_id REFERENCES produto(id)
 	PRIMARY KEY(id)
 )
+
+CREATE TABLE usuario (
+    ID SERIAL,
+    nome varchar(30) NOT NULL,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE categoria (
+    ID SERIAL,
+	nome varchar(30),
+    PRIMARY KEY(ID)
+);
+
+CREATE TABLE produto (
+    ID SERIAL,
+    categoria_id BIGINT UNSIGNED NOT NULL,
+    preco double NOT NULL,
+    prazo integer DEFAULT 0 NOT NULL,
+    avaliacao double DEFAULT 0 NOT NULL,
+    nome varchar(30) NOT NULL,
+    PRIMARY KEY (ID)
+);
+
+
+CREATE TABLE fornecedor (
+	id SERIAL,
+	nome varchar(255),
+	eh_local boolean,
+	email varchar(30),
+	celular varchar(30),
+	cidade varchar(30),
+	site varchar(255),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE fornecedor_produto (
+	id SERIAL,
+	fornecedor_id BIGINT UNSIGNED NOT NULL,
+	produto_id BIGINT UNSIGNED NOT NULL,
+	FOREIGN KEY (fornecedor_id) REFERENCES fornecedor(id),
+	FOREIGN KEY (produto_id) REFERENCES produto(id),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE produto_categoria (
+	id SERIAL,
+	produto_id BIGINT UNSIGNED NOT NULL,
+	category_id BIGINT UNSIGNED NOT NULL,
+	FOREIGN KEY (produto_id) REFERENCES produto(id),
+	FOREIGN KEY (category_id) REFERENCES categoria(id),
+	PRIMARY KEY(id)
+);
+
+create table interacao (
+	id SERIAL,
+	tempo_entrega varchar(255),
+	preço float,
+	qualidade varchar(255),
+	pagamento varchar(255),
+	fornecedor_id BIGINT UNSIGNED NOT NULL,
+	FOREIGN KEY (fornecedor_id) references fornecedor(id),
+	PRIMARY KEY(id)
+)
+
+create table interacao_produto (
+	id SERIAL,
+	interacao_id BIGINT UNSIGNED NOT NULL,
+	produto_id BIGINT UNSIGNED NOT NULL,
+	quantidade integer NOT NULL,
+	FOREIGN KEY (interacao_id) REFERENCES interacao(id),
+	FOREIGN KEY (produto_id) REFERENCES produto(id),
+	PRIMARY KEY(id)
+)
+
+
+select * from categoria;
+
+ALTER TABLE produto 
+modify avaliacao double
+
+
+
+
+
+
